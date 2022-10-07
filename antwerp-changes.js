c4d34cd4f94e76c8456a4c08398af2c88dc007cc
catpea <corporate.miou@gmail.com>
2022-10-07 17:25:41 -0400

    New Release

--- antwerp-changes.js
+++ antwerp-changes.js
@@ -8,12 +8,9 @@ import conf from './util/conf.js';
 import {camelCase, merge} from 'lodash-es';
 import frontMatter from 'front-matter';
 const program = new Command();
-program.option('-f, --force', 'force installation');
-// program.requiredOption('-s, --source [name]', 'specify source database');
 program.option('-t, --title [name]', 'specify title');
 program.option('-a, --after [name]', 'specify position');
 program.option('-d, --debug', 'debug or dry run where nothing is changed.');
-program.option('-s, --silent', 'debug stuff, don\'t print payload.');
 program.parse(process.argv);
 
 import npmConf from 'conf';
@@ -24,19 +21,16 @@ const configuration = path.join( c.get(project), 'conf.js' );
 
 const opts = program.opts();
 if (!conf.length) { console.error('project name required'); process.exit(1); }
-const options = Object.fromEntries(['force', 'debug'].map(key=>([key, opts[key]])))
+const options = Object.fromEntries(['debug'].map(key=>([key, opts[key]])))
 const config = await conf(configuration, options);
 
-
-
+const locations = [];
 for (const profile of config.publish.profiles) {
   const prescription = await rsend(merge({}, profile, { src: { dir: config.configuration.dest }}), config.configuration);
-  // console.log(prescription.script.join('\n'));
-  if (opts.silent){
-    // don't print
-  }else{
-    console.log(prescription.script);
-  }
+  const location = path.resolve([profile.name, profile.batchfile].join('-'));
+  await fs.writeFile(location, prescription.script.trim() + '\n');
+  locations.push(location);
 }
+console.log(locations.map(i=>JSON.stringify(i)).join(' '));
 
  
