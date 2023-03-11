c1b06dfa94a13c15e4b95b885fa126580a37ac17
catpea <corporate.miou@gmail.com>
2023-03-11 12:08:42 -0500

    Improve plain text generation.

--- util/functions.js
+++ util/functions.js
@@ -1,4 +1,5 @@
 import { convert } from 'html-to-text';
+import cheerio from 'cheerio';
 import moment from 'moment';
 import numeral from 'numeral';
 import classes from 'classnames';
@@ -9,22 +10,34 @@ export function iff(data, payload){
   const result = data.value();
   if(result) payload(result);
 }
+// export function plaintext(html){
+//   return convert(html, {
+//     wordwrap: false,
+//     selectors: [
+//       { selector: 'hr', format: 'skip' },
+//       // { selector: 'a',  format: 'skip'},
+//       { selector: 'img', format: 'skip' },
+//       { selector: 'h1', format: 'skip' },
+//       { selector: 'h2', format: 'skip' },
+//       { selector: 'h3', format: 'skip' },
+//       { selector: 'h4', format: 'skip' },
+//       { selector: 'h5', format: 'skip' },
+//       { selector: 'h6', format: 'skip' },
+//     ],
+//   })
+// }
+
 export function plaintext(html){
-  return convert(html, {
-    wordwrap: false,
-    selectors: [
-      { selector: 'hr', format: 'skip' },
-      { selector: 'a',  format: 'skip'},
-      { selector: 'img', format: 'skip' },
-      { selector: 'h1', format: 'skip' },
-      { selector: 'h2', format: 'skip' },
-      { selector: 'h3', format: 'skip' },
-      { selector: 'h4', format: 'skip' },
-      { selector: 'h5', format: 'skip' },
-      { selector: 'h6', format: 'skip' },
-    ],
-  })
+  let response = "";
+
+  if(html){
+    const $ = cheerio.load(html);
+    response = $.text();
+  }
+
+  return response;
 }
+
 export function hostname(url){
 
   if(url.startsWith('/')){
